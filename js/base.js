4cb2c561aa78d407b9de08da2bb9110a4a2d548a
Alexis <e1577094@cmaisonneuve.qc.ca>
2021-01-20 16:55:43 -0500

    Premier commit

---
+++ js/base.js
@@ -0,0 +1,294 @@
+// VARIABLES
+
+
+var buttonIndice = document.querySelector("#pagequestionNovice #buttonindice"); //btn
+var buttonIndice2 = document.querySelector("#section3 #buttonindice2"); //btn
+var buttonSuivant = document.querySelector("div #buttonsuivant"); //btn
+var buttonSuivant2 = document.querySelector("div #buttonsuivant2"); //btn
+
+var pageniveau = document.querySelector("#pageniveau");   //page
+var pagequestionNovice = document.querySelector("#pagequestionNovice"); //page
+var pagequestionExpert = document.querySelector("#pagequestionExpert"); //page
+
+var titreNiveauExpert = document.querySelector("#spanNiveau2"); //qst
+var pIndice = document.querySelector("#pagequestionNovice #pIndice"); //qst
+var titreNumeroQuestion = document.querySelector("div h1 #spanNumero2"); //qst
+var elementQuestion = document.querySelector("div p"); //qst
+var elementReponse1 = document.querySelector("div button.rp1"); //qst
+var elementReponse2 = document.querySelector("div button.rp2"); //qst
+var elementReponse3 = document.querySelector("div button.rp3"); //qst
+var elementReponse4 = document.querySelector("div button.rp4"); //qst
+var pointage = 0;
+
+var pIndiceExp = document.querySelector("#section3 #pIndice2"); //qst
+var elementQuestionExp = document.querySelector("#divDuQuestionExpert #pQuestionExpert"); //qst
+var elementReponse1exp = document.querySelector("#divDuQuestionExpert button.rp1"); //qst
+var elementReponse2exp = document.querySelector("#divDuQuestionExpert button.rp2"); //qst
+var elementReponse3exp = document.querySelector("#divDuQuestionExpert button.rp3"); //qst
+var elementReponse4exp = document.querySelector("#divDuQuestionExpert button.rp4"); //qst
+
+var pageresultat = document.querySelector("#pageresultatReussi"); //rst
+var pageresultatAband = document.querySelector("#pageresultatAbandonne"); //rst
+var buttonAbandonnerQuestionNovice = document.querySelector("div #buttonabadonner"); //rst
+var pointageAbandonne = document.querySelector("#pageresultatAbandonne #spanpointage"); //rst
+var pourcentageAbandonne = document.querySelector("#pageresultatAbandonne #spanpourcentage"); //rst
+ 
+var bonneReponseCourante;
+var reponseChoisie = null;
+/**
+ * Pour changer les question aléatoirement.
+*/
+
+titreNiveauNovice = document.querySelector("#pagequestionNovice #spanNiveau").textContent;
+
+function changerQuestionFacile() {
+
+    let positionAleaFac = Math.floor(Math.random()*choixquestion.facile.length);
+
+    let questionAleatoire = choixquestion.facile[positionAleaFac]; 
+    let texteIndice = choixquestion.facile[positionAleaFac].indice;
+        elementQuestion.innerHTML = questionAleatoire.titre;
+        pIndice.style.display = "none";
+        pIndice.innerHTML = texteIndice;
+        elementReponse1.innerHTML = questionAleatoire.reponses[0];
+        elementReponse2.innerHTML = questionAleatoire.reponses[1];
+        elementReponse3.innerHTML = questionAleatoire.reponses[2];
+        elementReponse4.innerHTML = questionAleatoire.reponses[3];
+        bonneReponseCourante = questionAleatoire.bonneReponse;
+        choixquestion.facile.splice([positionAleaFac], 1);
+}
+
+function changerQuestionIntermediaireEtFacile() {
+    let positionAlea = Math.floor(Math.random()*choixquestion.intermediaire.length);
+    let questionAleatoire = choixquestion.intermediaire[positionAlea];
+    let texteIndice = choixquestion.intermediaire[positionAlea].indice;
+    
+        elementQuestion.innerHTML = questionAleatoire.titre;
+        pIndice.style.display = "none";
+        pIndice.innerHTML = texteIndice;
+        elementReponse1.innerHTML = questionAleatoire.reponses[0];
+        elementReponse2.innerHTML = questionAleatoire.reponses[1];
+        elementReponse3.innerHTML = questionAleatoire.reponses[2];
+        elementReponse4.innerHTML = questionAleatoire.reponses[3];
+        bonneReponseCourante = questionAleatoire.bonneReponse; 
+        choixquestion.intermediaire.splice([positionAlea], 1);
+}
+
+function changerQuestionIntermediaireEtDifficle() {
+    let positionAlea = Math.floor(Math.random()*choixquestion.intermediaire.length);
+    let questionAleatoire = choixquestion.intermediaire[positionAlea];
+    let texteIndice = choixquestion.intermediaire[positionAlea].indice;
+    
+        elementQuestionExp.innerHTML = questionAleatoire.titre;
+        pIndiceExp.style.display = "none";
+        pIndiceExp.innerHTML = texteIndice;
+        elementReponse1exp.innerHTML = questionAleatoire.reponses[0];
+        elementReponse2exp.innerHTML = questionAleatoire.reponses[1];
+        elementReponse3exp.innerHTML = questionAleatoire.reponses[2];
+        elementReponse4exp.innerHTML = questionAleatoire.reponses[3];
+        bonneReponseCourante = questionAleatoire.bonneReponse;
+        choixquestion.intermediaire.splice([positionAlea], 1);
+}
+
+function changerQuestionDifficile() {
+    let positionAlea = Math.floor(Math.random()*choixquestion.difficile.length);
+    let questionAleatoire = choixquestion.difficile[positionAlea];
+    let texteIndice = choixquestion.difficile[positionAlea].indice;
+        elementQuestionExp.innerHTML = questionAleatoire.titre;
+        pIndiceExp.style.display = "none";
+        pIndiceExp.innerHTML = texteIndice;
+        elementReponse1exp.innerHTML = questionAleatoire.reponses[0];
+        elementReponse2exp.innerHTML = questionAleatoire.reponses[1];
+        elementReponse3exp.innerHTML = questionAleatoire.reponses[2];
+        elementReponse4exp.innerHTML = questionAleatoire.reponses[3];
+
+        bonneReponseCourante = questionAleatoire.bonneReponse;
+        choixquestion.difficile.splice([positionAlea], 1);
+        
+}
+
+
+
+
+/**
+ * Pour changer la page Acceuil à la page Niveau
+*/ 
+var changehtmlAcceuilANiveau = document.querySelector("#pageacc .stylebutton");
+
+changehtmlAcceuilANiveau.addEventListener("click", function(){
+    pageacc.style.display = "none";
+    pageniveau.style.display = "block";
+});
+
+
+/**
+ * Pour changer la page Niveau à la page Question Novice 
+*/ 
+var changehtmlNiveauAQuestionNovice = document.querySelector("#section2 #button2");
+
+    changehtmlNiveauAQuestionNovice.addEventListener("click", function(){
+    pageniveau.style.display = "none";
+    pagequestionNovice.style.display = "block";
+    changerQuestionFacile();
+    titreNiveauNovice.innerHTML = "Facile";
+
+    // POUR AFFICHER L'INDICE
+
+buttonIndice.addEventListener("click", function(){
+    pIndice.style.display = "block";
+})
+});
+
+
+
+
+
+/**
+ * Pour Changer le numéro de la question en novice quand on click le button suivant.
+*/  
+    
+    var titreNiveauNovice = document.querySelector("#spanNiveau");
+    var NombreQuestion = 1;
+    var NombreDePoint = 0;
+
+
+    buttonSuivant.addEventListener("click",function(){
+        
+        if(reponseChoisie == bonneReponseCourante){
+            NombreDePoint++;
+        }
+        
+        if(NombreQuestion < 3){
+            changerQuestionFacile();
+            NombreQuestion++;
+            titreNiveauNovice.innerHTML = "Facile";
+        }
+
+        else if(NombreQuestion <= 4){
+            changerQuestionIntermediaireEtFacile();
+            NombreQuestion++;
+            titreNiveauNovice.innerHTML = "Intermediaire";
+    }
+        else
+        {
+            pagequestionNovice.style.display = "none";
+            pageresultat.style.display = "block";
+            spanpointage2.innerHTML = NombreDePoint;
+            spanpourcentage2.innerHTML = NombreDePoint * 20;
+        }
+
+    document.querySelector("span").innerHTML = NombreQuestion;
+});
+
+
+/** 
+ * Pour changer la page Niveau à la page Question Expert
+*/
+var changehtmlNiveauAQuestionExpert = document.querySelector("#section2 #button3");
+
+changehtmlNiveauAQuestionExpert.addEventListener("click", function(){
+    
+    pageniveau.style.display = "none";
+    pagequestionExpert.style.display = "block";
+    changerQuestionDifficile();
+    titreNiveauExpert.innerHTML = "Difficile";
+
+buttonIndice2.addEventListener("click", function(){
+pIndiceExp.style.display = "block";
+
+
+})
+});
+
+/**
+ * Pour changer le numéro de la question en expert quand on click le button suivant.
+*/ 
+
+let positionAleaintermediaire = Math.floor(Math.random()*choixquestion.intermediaire.length);
+let positionAleaDifficle = Math.floor(Math.random()*choixquestion.difficile.length);
+var NombreQuestion2 = 1;
+
+
+buttonSuivant2.addEventListener("click",function(){
+    
+    if(reponseChoisie2 == bonneReponseCourante){
+        NombreDePoint++;
+    }
+
+    if(NombreQuestion2 < 3){
+        changerQuestionDifficile();
+        NombreQuestion2++;
+        titreNiveauExpert.innerHTML = "Difficile";
+    }
+
+    else if(NombreQuestion2 <= 4){
+        changerQuestionIntermediaireEtDifficle();
+        NombreQuestion2++;
+        titreNiveauExpert.innerHTML = "Intermediaire";
+
+    }
+
+    else
+        {
+            pagequestionExpert.style.display = "none";
+            pageresultat.style.display = "block";
+            spanpointage2.innerHTML = NombreDePoint;
+            spanpourcentage2.innerHTML = NombreDePoint * 20;
+        }
+    
+    titreNumeroQuestion.innerHTML = NombreQuestion2;
+
+});
+
+// Aller a la page résultat quand on click le button abandonner de la page novice.
+
+
+buttonAbandonnerQuestionNovice.addEventListener("click", function(){
+    pagequestionNovice.style.display = "none";
+    pageresultatAband.style.display = "block";
+    pointageAbandonne.innerHTML = 0;
+    pourcentageAbandonne.innerHTML = 0;
+});
+
+
+// Aller au page résultat quand on click le button abandonner de la page expert.
+var buttonAbandonnerQuestionExpert = document.querySelector("div #buttonabadonner2");
+var pointageAbandonne = document.querySelector("#pageresultatAbandonne #spanpointage");
+var pourcentageAbandonne = document.querySelector("#pageresultatAbandonne #spanpourcentage");
+
+buttonAbandonnerQuestionExpert.addEventListener("click", function(){
+    pagequestionExpert.style.display = "none";
+    pageresultatAband.style.display = "block";
+    pointageAbandonne.innerHTML = 0;
+    pourcentageAbandonne.innerHTML = 0;
+});
+
+
+// Pour css rester en jaune quand on click
+var lesButtonsNovice = document.getElementById("lesButtonNovice");
+var resultat = document.querySelector("#pageresultat #spanpointage");
+var buttonsNovice = lesButtonsNovice.getElementsByClassName("button");
+var lesButtonsExpert = document.getElementById("divDuQuestionExpert");
+var lesButtonsExpert = lesButtonsExpert.getElementsByClassName("button2")
+
+
+
+ // POINTAGE
+
+for (let i=0; i<buttonsNovice.length; i++){
+    buttonsNovice[i].addEventListener("click", function(){  
+        reponseChoisie = i;
+        var buttonchoisi = document.getElementsByClassName("active");
+        buttonchoisi[0].className = buttonchoisi[0].className.replace("active", "");
+        this.className += " active";
+    });
+}
+
+for (let i=0; i<lesButtonsExpert.length; i++){
+    lesButtonsExpert[i].addEventListener("click", function(){  
+        reponseChoisie2 = i;
+        var buttonchoisi2 = document.getElementsByClassName("active2");
+        buttonchoisi2[0].className = buttonchoisi2[0].className.replace("active2", "");
+        this.className += " active2";
+    });
+}
---
+++ js/question.data.js
@@ -0,0 +1,225 @@
+var choixquestion = {
+    facile:[
+                    {
+                     titre:"Lequel de ces animaux ne fait pas partie du zodiaque chinois ?",
+                     niveau : "facile",
+                     reponses : [
+                        "Le singe",
+                        "Le chat",
+                        "Le tigre",
+                        "Le cochon"
+                        ],
+                        bonneReponse: 1,
+                    indice : "Deux félins dans le zodiaque ? Mmmh, bizzard..."
+                    },
+                    {
+                    titre:"Quelle est la capitale de la Chine ?",
+                    niveau : "facile",
+                    reponses : [
+                        "Beijing",
+                        "Wuhuan",
+                        "Shanghai",
+                        "Canton"
+                        ],
+                        bonneReponse: 0,
+                    indice : "En occident, on prononce le nom différement, et en commençant avec un 'P'."
+                    },
+                    {
+                    titre:"Quel jour de fête n’est pas officiel chez les Chinois ?",
+                    niveau : "facile",
+                    reponses : [
+                        "La fête du travail",
+                        "Noël",
+                        "Le festival des bateaux-dragons",
+                        "Le jour national"
+                        ],
+                        bonneReponse: 1,
+                    indice : "Ils préfèrent davantage les dragons que les barbes blanches." 
+                    },
+                    {
+                    titre:"Quel est le plat traditionnellement servi pendant le nouvel an chinois ?",
+                    niveau : "facile",
+                    reponses: [
+                        "Nouilles de longue-vie",
+                        "Légumes sautés",
+                        "Dumplings",
+                        "Baguette de pain"
+                        ],
+                        bonneReponse: 2,
+                    indice : "Délicieux contenu enrobé de mince pâte."
+                    },
+                    {
+                    titre:"Plusieurs arts martiaux chinois sont basés sur : ",
+                    niveau : "facile",
+                    reponses: [
+                        "Les animaux",
+                        "Les créatures marines",
+                        "L’architecture",
+                        "Les plantes"
+                        ],
+                        bonneReponse: 0,
+                    indice : "Kung Fu panda est un bon filme."
+                    },
+                    {
+                    titre:"Qu’est-ce que les Chinois utilisent traditionnellement pour mettre leur nom sur un document officiel ?",
+                    niveau : "facile",
+                    reponses: [
+                        "Un empreinte de doigt",
+                        "Une signature écrite",
+                        "Un autocollant",
+                        "Un sceau"
+                        ],
+                        bonneReponse: 3,
+                    indice : "De la cire d'oreil."
+                    }
+                   
+            ],
+    intermediaire: [
+                    {
+                    titre:"Combien de corde compte le fameux erhu (violon chinois) ?",
+                    niveau : "intermediaire",
+                    reponses: [
+                        "8",
+                        "4",
+                        "2",
+                        "1"
+                        ],
+                        bonneReponse: 2,
+                    indice : "Moins que la pluspart des violons que l'on connait en tous cas."
+                    },
+                    {
+                    titre:"Les gens de la Chine tendent à se référer à quel genre de pratique soignante avant de chercher de l’aide médicale ?",
+                    niveau : "intermediaire",
+                    reponses: [
+                        "Prières",
+                        "Herboristerie et acuponcture",
+                        "Marijuana",
+                        "Exercices et diète"
+                        ],
+                        bonneReponse: 1,
+                    indice : "Cette/ces pratiques(s) commence(nt) à être plus reconnu dans les Amériques."
+                    },
+                    {
+                    titre:"Quelle est la forme de peinture étant considérée, selon plusieurs critiques, comme la plus haute forme de peinture Chinoise ?",
+                    niveau : "intermediaire",
+                    reponses: [
+                        "Le paysage",
+                        "Le portrait",
+                        "Royale",
+                        "Mystique"
+                        ],
+                        bonneReponse: 0,
+                    indice : "Under The Great Wave Off Kanagawa"
+                    },
+                    {
+                    titre:"L’histoire de la Chine est divisée en quoi ?",
+                    niveau : "intermediaire",
+                    reponses: [
+                        "ères",
+                        "Régimes",
+                        "âges",
+                        "Dynasties"
+                        ],
+                        bonneReponse: 3,
+                    indice : "C'est plutôt potitiquo-familial."
+                    },
+                    {
+                    titre:"Durant les jours de fête et les événements spéciaux, les Chinois offrent souvent des enveloppes rouges à leurs amis et à leurs familles. Que retrouve-t-on dans ces enveloppes ?",
+                    niveau : "intermediaire",
+                    reponses: [
+                        "Des pépites d’or",
+                        "Des tickets",
+                        "Des feuilles de thé",
+                        "De l’argent"
+                        ],
+                        bonneReponse: 3,
+                    indice : "Il ne faut pas chercher trop loin..."
+                    },
+                    {
+                    titre:"Quelle fête Chinoise est célébré chaque 1er octobre ?",
+                    niveau: "intermediaire",
+                    reponses: [
+                        "La fête Nationale",
+                        "Le festival des bateaux-dragons" ,
+                        "Le jour des enfants",
+                        "Le festival de mi-automne"
+                        ],
+                        bonneReponse: 0,
+                    indice : "Haut le drapeau !"
+                    },
+                ],
+    difficile : [
+                    {
+                    titre:"Au cours des 2500 dernières années, les croyances religieuses et éthiques de la culture Chinoise ont été dominées par une philosophie bien distincte. Son influence se reflète encore dans tous les aspects majeurs de cette culture. Comment nomme-t-on cette philosophie/religion ?",
+                    niveau : "difficile",
+                    reponses: [
+                        "Le Zen",
+                        "Le confucianisme",
+                        "Le Maoïsme",
+                        "Le Gongbi"
+                        ],
+                        bonneReponse: 1,
+                    indice : "Presque entièrement influencé par le taoïsme"
+                    },
+                    {
+                    titre:"Laquelle de ces personnalités chinoises du cinéma n’apparaît pas dans un film de kung-fu ?",
+                    niveau : "difficile",
+                    reponses: [
+                        "Aaron Kwok",
+                        "Bruce Li",
+                        "Michelle Yeoh",
+                        "Jet Li"
+                        ],
+                        bonneReponse: 0,
+                    indice : "Celui-ci est plutôt un chanteur et un danseur."
+                    },
+                    {
+                    titre:"Le mandarin est le dialecte le plus courant en Chine mais est loin d’être le seul. Combien de dialectes Chinois principaux se retrouvent en Chine ?",
+                    niveau: "difficile",
+                    reponses: [
+                        "3",
+                        "22",
+                        "7",
+                        "5"
+                        ],
+                        bonneReponse: 2,
+                    indice : "Un bon nombre mais pas trop..."
+                    },
+                    {
+                    titre:"À quel jour de l’année a lieu le nouvel an Chinois ?",
+                    niveau: "difficile",
+                    reponses: [
+                        "1er février",
+                        "28 décembre",
+                        "1er octobre",
+                        "14 février"
+                        ],
+                        bonneReponse: 3,
+                    indice : "Mmmmmh, pas façile comme question..."
+                    },
+                    {
+                    titre:"Habituellement, en famille, les Chinois mangent sur : ",
+                    niveau: "difficile",
+                    reponses: [
+                        "Une table carrée",
+                        "Une table triangulaire",
+                        "Une table ronde",
+                        "Une table rectangulaire"
+                        ],
+                        bonneReponse: 2,
+                    indice : "Ça rapproche plus les gens."
+                    },
+                    {
+                    titre:"Quel nombre est considéré comme malchanceux en Chine ?",
+                    niveau: "difficile",
+                    reponses: [
+                        "4",
+                        "13",
+                        "7",
+                        "6"
+                        ],
+                        bonneReponse: 0,
+                    indice : "1, 2, 3, mort."
+                    },
+                ],  
+    };  
